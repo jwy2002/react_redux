2fc898149c0051b678fd02ba36e693fcba33963b
jwy2002 <50856876+jwy2002@users.noreply.github.com>
2021-04-19 17:11:24 +0900

    not store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot"
+import DisplayRoot from "./components/DisplayRoot"
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component {
+  state = {number:0}
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function(size){
+          this.setState({number:this.state.number + size});
+        }.bind(this)}></AddNumberRoot>
+        <DisplayRoot number={this.state.number}></DisplayRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
